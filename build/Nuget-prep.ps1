a459b80128aa40c9849cbcc4d0ffdaa05e833734
Niadapa <niadapa@microsoft.com>
2019-05-01 11:27:59 -0700

    added checks to ensure directories exist

--- build/Nuget-prep.ps1
+++ build/Nuget-prep.ps1
@@ -9,18 +9,28 @@ $NugetPackager = $RootPath + '\SceneLoader\NugetPackager\'
 $nupkg = $RootPath + '\bin\nupkg'
 
 cd $SceneLoader
+If(!(Test-Path $lib)) {
+    New-Item -ItemType Directory -Force -Path $lib
+}
 copy-item -path SceneLoaderComponent.idl -destination $lib
 
 cd $outputDirRelease
 copy-item -path SceneLoaderComponent.winmd -destination $lib
 
 cd $outputDirRelease
+If(!(Test-Path $runtimes)) {
+    New-Item -ItemType Directory -Force -Path $runtimes
+}
 copy-item -path SceneLoaderComponent.pri -destination $runtimes
 copy-item -path SceneLoaderComponent.dll -destination $runtimes
 
 cd $NugetPackager
 remove-item *.nupkg
 nuget pack
+
+If(!(Test-Path $nupkg)) {
+    New-Item -ItemType Directory -Force -Path $nupkg
+}
 copy-item -path *.nupkg -destination $nupkg
 
 cd $PSScriptRoot

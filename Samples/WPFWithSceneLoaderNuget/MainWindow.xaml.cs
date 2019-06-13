4cb2711db65aad754f020445e29a178b38494189
Niadapa <niadapa@microsoft.com>
2019-06-13 16:56:12 -0700

    add wpf sample files. Sceneloader dll forwarding wip.

--- Samples/WPFWithSceneLoaderNuget/MainWindow.xaml.cs
+++ Samples/WPFWithSceneLoaderNuget/MainWindow.xaml.cs
@@ -1,19 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿//  ---------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+// 
+//  The MIT License (MIT)
+// 
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+// 
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+// 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//  ---------------------------------------------------------------------------------
 
-namespace WPFWithSceneLoaderNuget
+using System;
+using System.Windows;
+
+namespace HelloComposition
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +35,15 @@ namespace WPFWithSceneLoaderNuget
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            float size = random.Next(50, 150);
+            float offsetX = random.Next(0, (int)(CompositionHostControl1.ActualWidth - size));
+            float offsetY = random.Next(0, (int)(CompositionHostControl1.ActualHeight/2 - size));
+            CompositionHostControl1.AddElement(size, offsetX, offsetY);
         }
     }
 }
--- Samples/WPFWithSceneLoaderNuget/Properties/AssemblyInfo.cs
+++ Samples/WPFWithSceneLoaderNuget/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WPFWithSceneLoaderNuget")]
+[assembly: AssemblyTitle("HelloComposition")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WPFWithSceneLoaderNuget")]
+[assembly: AssemblyProduct("HelloComposition")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

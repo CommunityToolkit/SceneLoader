a2e3f97c64ede4be41f7e3f2a0f0d2e0f9c83fe7
Patrick Egan <t-paega@microsoft.com>
2019-08-22 11:49:02 -0700

    removed some unnecessary using statements

--- Experimental/CameraComponent/ModelViewer.xaml.cs
+++ Experimental/CameraComponent/ModelViewer.xaml.cs
@@ -3,19 +3,9 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using SceneLoaderComponent;
 using Windows.UI.Composition;
 using Windows.UI.Composition.Scenes;
